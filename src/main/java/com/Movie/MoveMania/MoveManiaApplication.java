5a75f25e5f3098497c26ce1d3781ba2ecb3d9a7b
15josh08 <joshrajendrapatel@gmail.com>
2024-09-08 16:01:27 -0400

    The MovieMania project for movie lovers.

---
+++ src/main/java/com/Movie/MoveMania/MoveManiaApplication.java
@@ -0,0 +1,13 @@
+package com.Movie.MoveMania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveManiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Movie/MoveMania/MoveManiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Movie.MoveMania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveManiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
