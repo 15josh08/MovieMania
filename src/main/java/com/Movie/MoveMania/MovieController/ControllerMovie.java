bc98e9e65a5759a859c425026fad5ee73afe0261
15josh08 <joshrajendrapatel@gmail.com>
2024-09-08 19:40:28 -0400

    Added search by  id

--- src/main/java/com/Movie/MoveMania/MovieController/ControllerMovie.java
+++ src/main/java/com/Movie/MoveMania/MovieController/ControllerMovie.java
@@ -1,13 +1,16 @@
 package com.Movie.MoveMania.MovieController;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.service.annotation.GetExchange;
@@ -25,5 +28,10 @@ public class ControllerMovie {
     @GetMapping("/allmovies")
     public ResponseEntity<List<Movie>> allMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Movie> getMovieById(@PathVariable ObjectId id) {
+        return movieService.getMovieById(id);
     }
 }
--- src/main/java/com/Movie/MoveMania/Service/MovieService.java
+++ src/main/java/com/Movie/MoveMania/Service/MovieService.java
@@ -1,7 +1,9 @@
 package com.Movie.MoveMania.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +20,9 @@ public class MovieService {
     public List<Movie> allMovies() {
         return movieRepository.findAll();
     }
+
+    public Optional<Movie> getMovieById(ObjectId id) {
+        return movieRepository.findById(id);
+    }
+
 }
